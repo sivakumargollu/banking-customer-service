249e040bf799b6c6f0895f4b8c48edecc8de1cc7
Siva Kumar <sivakumargollu@gmail.com>
2018-02-21 18:28:47 +0530

    Operator details changed to entity

--- src/main/java/com/abcbank/counter/service/models/OperatorDetails.java
+++ src/main/java/com/abcbank/counter/service/models/OperatorDetails.java
@@ -3,16 +3,29 @@ package com.abcbank.counter.service.models;
 import com.abcbank.counter.service.enums.Previlages;
 import com.abcbank.counter.service.enums.Role;
 
+import javax.persistence.*;
 import java.util.List;
 import java.util.Map;
 
+@Entity
 public class OperatorDetails {
 
-	String operatorId;
-	Role   role;
-	Map    previliages;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	Long Id;
 
-	public OperatorDetails(String operatorId, Role role, Map<String, List<Previlages>> accessPreviliages) {
+	@Column
+	String operatorId;
+
+	@Enumerated(EnumType.STRING)
+	@Column
+	Role   role;
+
+	@ElementCollection
+	@Column
+	Map<String, Previlages[]>  previliages;
+
+	public OperatorDetails(String operatorId, Role role, Map<String, Previlages[]> accessPreviliages) {
 		this.previliages = accessPreviliages;
 		this.operatorId = operatorId;
 		this.role = role;
